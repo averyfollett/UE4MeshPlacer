2b87708f820706e5424472de84793baafd4ef373
Avery Follett <avery.follett@mymail.champlain.edu>
2020-04-16 11:36:02 -0400

    Made Unreal project C++ instead of blueprint

---
+++ Source/Graphics2Final/Graphics2Final.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Graphics2Final.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Graphics2Final, "Graphics2Final" );
